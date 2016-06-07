3ce2f3fd64c928460ab23fc715c0a9ade6fa993a
Lacey Powers <lacey.leanne@gmail.com>
2016-06-07 13:31:58 -0700

    Added a defaultargs parameter to this script, allowing a different value to be passed or if none is passed, default to the original 10100 events captured. Added copyright for Brendan Gregg per upstream pull request feedback.

--- FS/zilt10k.d
+++ FS/zilt10k.d
@@ -9,18 +9,43 @@
  * It ideally captures at least 10,000 I/O events.  It has
  * a 15 minute timeout if that is not possible.
  *
+ * Copyright (c) 2013 Brendan Gregg. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
  * 25-Jun-2013	Brendan Gregg	Created this.
  * 23-Jan-2015  Lacey Powers    Updated for FreeBSD
  */
 
 #pragma D option quiet
 #pragma D option switchrate=5
+#pragma D option defaultargs
 
 BEGIN
 {
 	printf("ENDTIME(us) LATENCY(ns) TYPE SIZE(bytes) PROCESS\n");
 	start = timestamp;
 	n = 0;
+	times = $1 != NULL ? $1 : 10100;
 }
 
 fbt::zfs_freebsd_read:entry, fbt::zfs_freebsd_write:entry
@@ -41,7 +66,7 @@ fbt::zfs_freebsd_readdir:entry, fbt::zfs_freebsd_getattr:entry
 fbt::zfs_freebsd_read:entry, fbt::zfs_freebsd_write:entry,
 fbt::zfs_freebsd_open:entry, fbt::zfs_freebsd_close:entry,
 fbt::zfs_freebsd_readdir:entry, fbt::zfs_freebsd_getattr:entry
-/n++ > 10100/
+/n++ > times/
 {
 	exit(0);
 }
